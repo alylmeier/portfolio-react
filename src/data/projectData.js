248f3cbd42ee80a5d969203f56ab73c848f606a9
alylmeier <alylmeier@gmail.com>
2023-05-15 18:11:57 -0400

    trying tech again

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,10 +2,11 @@ const arr = [
     {name: "Breaking Dad", bio: "https://cellopudding.github.io/BreakingDad/", image: 'br image', GH:"https://github.com/cellopudding/BreakingDad" },
     {name: "Terrain of Thought",  bio: "https://shielded-temple-21395.herokuapp.com/", image: 'yosemite', GH: "https://github.com/Reptartheman/Terrain-of-Thought"},
     {name: "The Tech Blog",  bio: "https://tranquil-basin-07024.herokuapp.com/", image: 'tech-blog-ss', GH: "https://github.com/alylmeier/tech-blog"},
+    {name: "Tech Tickets", bio: "https://tech-tickets.herokuapp.com/#Home", image: 'techtickets', GH: "https://github.com/NeelCheo/tech_tickets" },
     {name: "Note Taker",  bio: "https://immense-sands-36851.herokuapp.com/", image: 'note taker ss', GH: "https://github.com/alylmeier/note-taker"},
     {name: "Weather Dashboard",  bio: "https://alylmeier.github.io/weather-dashboard/", image: 'weather', GH: "https://github.com/alylmeier/weather-dashboard"},
     {name: "Day Planner",  bio: "https://alylmeier.github.io/day-planner/", image: 'day planner', GH:"https://github.com/alylmeier/day-planner"},
-    {name: "Tech Tickets", bio: "https://tech-tickets.herokuapp.com/#Home", image: 'techtickets', GH: "https://github.com/NeelCheo/tech_tickets" },
+    
 ]
 
 export default arr
